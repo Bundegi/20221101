ab18b0afac8d20d51e8f77cfad23681edd1d58c7
Yonguk Kim <yonguk99@naver.com>
2022-11-01 16:50:25 +0900

    first commit

---
+++ 1699jun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
+int dp[100001] = { 0, };
+int main() {
+	fast;
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		dp[i] = i;
+		for (int j = 1; j*j<= i; j++)
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+	}
+	cout << dp[N] << endl;
+}
